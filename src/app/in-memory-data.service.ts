52b6df7bd9ddd8735ed9fa559840c55d2472c6a1
Adrian Albert Ciubucciu <aac@cloudlab.at>
2021-08-18 17:11:38 +0300

    fixing the button delete button and games width

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -8,6 +8,83 @@ import { Game } from './game';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const games = [
+      {
+        id: 1,
+        name: 'League Of Legends',
+        online: true,
+        releaseDate: '23-05-2010',
+        desiredName: ''
+      },
+      {
+        id: 2,
+        name: 'CS:GO',
+        online: true,
+        releaseDate: '',
+        desiredName: ''
+      },
+      {
+        id: 3,
+        name: 'Need For Speed Most Wanted 2',
+        online: false,
+        releaseDate: '12-07-2005',
+        desiredName: ''
+      },
+      {
+        id: 4,
+        name: 'PUBG',
+        online: true,
+        releaseDate: '11-08-2015',
+        desiredName: ''
+      },
+      {
+        id: 5,
+        name: 'The Witcher',
+        online: false,
+        releaseDate: '02-02-2007',
+        desiredName: ''
+      },
+      {
+        id: 6,
+        name: 'DOTA 2',
+        online: true,
+        releaseDate: '03-05-2010',
+        desiredName: ''
+      },
+      {
+        id: 7,
+        name: 'Max Payne 2',
+        online: false,
+        releaseDate: '22-12-2005',
+        desiredName: ''
+      },
+      {
+        id: 8,
+        name: 'Max Payne 3',
+        online: true,
+        releaseDate: '09-12-2000',
+        desiredName: ''
+      },
+      {
+        id: 9,
+        name: 'Call Of Duty: Warzone',
+        online: true,
+        releaseDate: '12-07-2017',
+        desiredName: ''
+      },
+      {
+        id: 10,
+        name: 'Fortnite',
+        online: true,
+        releaseDate: '05-05-2005',
+        desiredName: ''
+      },
+      {
+        id: 11,
+        name: 'Apex Legends',
+        online: true,
+        releaseDate: '18-10-2018',
+        desiredName: ''
+      },
       { id: 11, name: 'Forza Horizon 5'},
       { id: 12, name: 'Smite'},
       { id: 13, name: 'Dirt 3'},
@@ -16,7 +93,7 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 16, name: 'Warcraft 2'},
       { id: 17, name: 'Halo 2'},
       { id: 18, name: 'Fifa 21'},
-      { id: 19, name: 'Need For Speed: Heat'},
+      { id: 19, name: 'Need For Speed: Carbon'},
       { id: 20, name: 'Among Us'},
       { id: 21, name: 'Valorant'},
       { id: 22, name: 'Pleague Inc.'},
